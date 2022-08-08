705126efc1f618b94e995996cf3b05e581679134
JumaboyevJasurbek <100076086+JumaboyevJasurbek@users.noreply.github.com>
2022-08-08 18:58:33 +0500

    pages,img

--- src/pages/Ecommerce.js
+++ src/pages/Ecommerce.js
@@ -38,7 +38,6 @@ const Ecommerce = () => {
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center ">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3  bg-no-repeat bg-cover bg-center">
-          {/* <div class="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 rounded-xl  w-full p-8 pt-9 m-3 bg-no-repeat bg-cover bg-center"> */}
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
